85fd2b9083e064213205126825b1d28787b36fdb
Zakaria Ahmed <76578327+Zeko6666@users.noreply.github.com>
2024-06-25 18:41:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ document.addEventListener('gesturechange', function (e) {
 document.addEventListener('gestureend', function (e) {
     e.preventDefault();
 });
+
+document.addEventListener('keydown', function(event) {
+    if ((event.ctrlKey || event.metaKey) && (event.key === '+' || event.key === '-')) {
+        event.preventDefault();
+    }
+});
